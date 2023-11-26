1e54302ef7c8ef4d9d847ac6abe5ab929e83be1d
Dfc-jalen <146123090+Dfc-jalen@users.noreply.github.com>
2023-11-26 18:49:22 -0500

    Here is the work for the Python Queue simulator. I have this on replit as well if this file somehow doesn't run properly.

---
+++ PythonQueue.py
@@ -0,0 +1,19 @@
+names = [] 
+
+namecount = 0
+for i in range(10):
+  name = input('enter name: ')
+  print(' ')
+  names.append(name)
+  namecount += 1
+  print(namecount, 'names entered')
+  print(' ')
+
+print(names)
+print(' ')
+
+for i in range(10):
+  len(names)
+  print(' ')
+  print(names.pop(0))
+  
